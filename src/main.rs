3e2113e411cd51b4526ba1a029b1fcd4c421d07a
Erwin Villejo <erwinvillejo@gmail.com>
2023-05-26 15:53:58 +0800

    extract lib from bin

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,14 @@
+use exer;
+
 fn main() {
-    // Program entry point
-    collatz(6);
-
-    println!();
-    arrs();
-
-    println!();
-    tups();
-
-    println!();
-    refs();
-
-    // println!();
-    // danglingRefs();
-
-    println!();
-    slices();
-
-    println!();
-    strs();
-
-    println!();
-    print_fizzbuzz_to(20);
-}
-
-fn collatz(mut x: i32) {
-    // Mutable variable binding
-    print!("{x}"); // Macro for printing, like printf
-    while x != 1 {
-        // No parenthesis around expression
-        if x % 2 == 0 {
-            // Math like in other languages
-            x = x / 2;
-        } else {
-            x = 3 * x + 1;
-        }
-        print!(" -> {x}");
-    }
-    println!();
-}
-
-fn arrs() {
-    let mut a: [i8; 10] = [42; 10];
-    a[5] = 0;
-    println!("a: {a:?}");
-}
-
-fn tups() {
-    let t: (i8, bool) = (7, true);
-    println!("1st index: {}", t.0);
-    println!("2nd index: {}", t.1);
-}
-
-fn refs() {
-    let mut x: i32 = 10;
-    let ref_x: &mut i32 = &mut x;
-    *ref_x = 20;
-    println!("x: {x}");
-}
-
-/*
-fn danglingRefs() {
-    let ref_x: &i32;
-    {
-        let x: i32 = 10;
-        ref_x = &x;
-    }
-    println!("ref_x: {ref_x}");
-}
-*/
-
-fn slices() {
-    let a: [i32; 6] = [10, 20, 30, 40, 50, 60];
-    println!("a: {a:?}");
-
-    let s: &[i32] = &a[2..4];
-    // a[3] = 0;
-    println!("s: {s:?}");
-}
-
-fn strs() {
-    let s1: &str = "World";
-    println!("s1: {s1}");
-
-    let mut s2: String = String::from("Hello ");
-    println!("s2: {s2}");
-    s2.push_str(s1);
-    println!("s2: {s2}");
-
-    let s3: &str = &s2[6..];
-    println!("s3: {s3}");
-}
-
-fn is_divisible(n: u32, divisor: u32) -> bool {
-    if divisor == 0 {
-        return false;
-    }
-    n % divisor == 0
-}
-
-fn fizzbuzz(n: u32) -> String {
-    let fizz = if is_divisible(n, 3) { "fizz" } else { "" };
-    let buzz = if is_divisible(n, 5) { "buzz" } else { "" };
-    if fizz.is_empty() && buzz.is_empty() {
-        return format!("{n}");
-    }
-    format!("{fizz}{buzz}")
-}
-
-fn print_fizzbuzz_to(n: u32) {
-    for i in 1..=n {
-        println!("{}", fizzbuzz(i));
-    }
+    // exer::collatz(6);
+    // exer::arrs();
+    // exer::tups();
+    // exer::refs();
+    // exer::dangling_refs();
+    // exer::slices();
+    // exer::strs();
+    // exer::print_fizzbuzz_to(20);
+    // exer::rect();
+    exer::pick();
 }
